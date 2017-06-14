6d36ffa2c74b573174432f9f0fbcbc33bc551012
Reyn-L <Reyn.leong@gmail.com>
2017-06-13 17:15:01 -1000

    Revert "Finished"

--- scope.js
+++ scope.js
@@ -3,12 +3,10 @@ var isCookieJarOpen = null;
 
 function closeLid() {
   /* answer here */
-  isCookieJarOpen = false;
 }
 
 function openLid() {
   /* answer here */
-  isCookieJarOpen = true;
 }
 
 
@@ -19,7 +17,7 @@ function outerFunction() {
 
   function innerFunction() {
     var world = 'World';
-    return "Hello World";
+    return /* answer here */;
   }
   return innerFunction();
 }
@@ -33,8 +31,8 @@ function addMatrixElements(matrix) {
 
   for(var i = 0; i < matrix.length; i++) {
     /* fix counter variables in the second loop */
-    for(var j = 0; j < matrix[i].length; j++) {
-      result = result + matrix[i][j];
+    for(var i = 0; i < matrix[i].length; i++) {
+      result = result + matrix[i][i];
     }
   }
   return result;
@@ -47,8 +45,8 @@ function sendDataToClient() {
 
   var userObject = {
     handle: 'neo',
-    authenticated: true
-  };
+    authenticated: false
+  }
 
   function authenticateUser(obj, username) {
     var userObject = {
@@ -58,11 +56,11 @@ function sendDataToClient() {
 
     if (userObject.handle === username) {
       userObject.authenticated = true;
-      return userObject;
+      return userObject
     } else {
-      return userObject;
+      return userObject
     }
   }
-  authenticateUser(userObject, 'neo');
-  return userObject;
+  authenticateUser(userObject, 'neo')
+  return userObject
 }
